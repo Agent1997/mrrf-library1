99b72e9aaae8ad950fb77ffeddec043e29cacbd5
Rommel Malqued <52694054+Agent1997@users.noreply.github.com>
2023-07-29 19:34:07 -0400

    added dependency links

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 install_requires = []
 
-with open('requirements.txt', 'r', encoding='utf-8') as requirements_file:   
-    install_requires = requirements_file.readlines()
+# with open('requirements.txt', 'r', encoding='utf-8') as requirements_file:   
+#     install_requires = requirements_file.readlines()
       
 setup(
     name='mrrf_library_one',
     version='1.0.0',
     author='Agent 1997',
     packages=find_packages(exclude=['docs']),
-    install_requires=["git+https://github.com/Agent1997/mrrf-core.git@main"]
+    install_requires=install_requires,
+    dependency_links=["git+https://github.com/Agent1997/mrrf-core.git@main"]
 )
